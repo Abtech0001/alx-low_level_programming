841526e1615fb449313dde5d4ac1029791cbdee1
Lawal Abdulhameed Aremu <lawalabdulhameed91@gmail.com>
2023-09-08 14:25:33 -0700

    work

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	/* Print lowercase alphabet */
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	/* Print uppercase alphabet' */
+	lowercase = 'a';
+
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar('\n');  /* Print a new line */
+	return (0);
+}
+
