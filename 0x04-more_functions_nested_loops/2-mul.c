4797edab14d5a10c3ff393e179796fa0ae52ce96
Lawal Abdulhameed Aremu <lawalabdulhameed91@gmail.com>
2023-09-14 13:30:03 -0700

    wweel

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two number
+ * @a: number to be multiplied to b
+ * @b: number to be multiplied to a
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
